8673c9e0ca1bf190fd36c0763495a4b4fc197685
pratik gupta <prati1907@gmail.com>
2019-09-28 21:50:54 +0530

    Initial Commit

---
+++ Kindle App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindle App
+//
+//  Created by pratik gupta on 28/09/19.
+//  Copyright © 2019 pratik gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
